14b3b614e41191d3f818b38f4ed0d67783c789ea
Tyler Dressler <dresslertr@gmail.com>
2016-10-19 14:47:33 -0600

    Initial set up of scene and Game&Audio Manager

---
+++ Apocalypse Nations/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+// initial Author Tyler
+public class AudioManager {
+
+    #region Fields    
+    static AudioManager instance;
+    public AudioSource backgroundMusicAudioSource;
+    public AudioSource sfxAudioSource;
+    public AudioMixer masterAudioMixer;
+    public AudioMixer BackgroundAudioMixer;
+    public AudioMixer SoundEffectAudioMixer;
+    #endregion
+
+    #region Properites
+    public AudioManager Instance()
+    {
+        if (instance != null)
+        {
+            return new AudioManager();
+        }
+        else
+            return instance;
+    }
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

327e351256f8e9bf6ad7db2ec71855fcf66b2991
TimDHenderson <timdothenderson@gmail.com>
2016-10-19 15:15:39 -0600

    updated code

---
+++ Apocalypse%2520Nations/Assets/SaveLoad.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+	public class SaveLoad
+	{
+	public static List<nationData> nations = new List<nationData> ();
+		
+		public static void SaveNations(){
+		nations.Clear();
+		SaveLoad.nations.Add (nationData.current);
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/ChoiceCalcs.db", FileMode.OpenOrCreate);
+		bf.Serialize (file, nationData.current);
+			file.Close ();
+		}
+		public static void LoadNations()
+		{
+			if(File.Exists(Application.persistentDataPath+ "/ChoiceCalcs.db"))
+			{
+				BinaryFormatter bf = new BinaryFormatter ();
+				FileStream file = File.Open(Application.persistentDataPath +"/ChoiceCalcs.db", FileMode.Open);
+				SaveLoad.nations = (List<nationData>)bf.Deserialize(file);
+				file.Close();
+			}
+		}
+	public static void CalcNation(Nation nation, int apocalypse)
+	{
+		nation.allegience = nation.allegience * apocalypse;
+		nation.control = nation.control * apocalypse;
+		nation.military = nation.military * apocalypse;
+		nation.religion = nation.religion * apocalypse;
+		nation.science = nation.science * apocalypse;
+	}
+	public static void CalcNations(int apocalypse)
+	{
+		nations.Clear();
+		nations.Add (nationData.current);
+
+		CalcNation (nations[0].n1, apocalypse);
+		CalcNation (nations[0].n2, apocalypse);
+		CalcNation (nations[0].n3, apocalypse);
+		CalcNation (nations[0].n4, apocalypse);
+		CalcNation (nations[0].n5, apocalypse);
+		CalcNation (nations[0].n6, apocalypse);
+		CalcNation (nations[0].n7, apocalypse);
+		CalcNation (nations[0].n8, apocalypse);
+		CalcNation (nations[0].n9, apocalypse);
+		CalcNation (nations[0].n10, apocalypse);
+		CalcNation (nations[0].n11, apocalypse);
+		CalcNation (nations[0].n12, apocalypse);
+		CalcNation (nations[0].n13, apocalypse);
+		CalcNation (nations[0].n14, apocalypse);
+		CalcNation (nations[0].n15, apocalypse);
+		CalcNation (nations[0].n16, apocalypse);
+		CalcNation (nations[0].n17, apocalypse);
+		CalcNation (nations[0].n18, apocalypse);
+		CalcNation (nations[0].n19, apocalypse);
+		CalcNation (nations[0].n20, apocalypse);
+		CalcNation (nations[0].n21, apocalypse);
+		CalcNation (nations[0].n22, apocalypse);
+		CalcNation (nations[0].n23, apocalypse);
+		CalcNation (nations[0].n24, apocalypse);
+		CalcNation (nations[0].n25, apocalypse);
+		CalcNation (nations[0].n26, apocalypse);
+		CalcNation (nations[0].n27, apocalypse);
+		CalcNation (nations[0].n28, apocalypse);
+		CalcNation (nations[0].n29, apocalypse);
+		CalcNation (nations[0].n30, apocalypse);
+		CalcNation (nations[0].n31, apocalypse);
+		CalcNation (nations[0].n32, apocalypse);
+		CalcNation (nations[0].n33, apocalypse);
+		CalcNation (nations[0].n34, apocalypse);
+		CalcNation (nations[0].n35, apocalypse);
+		CalcNation (nations[0].n36, apocalypse);
+		CalcNation (nations[0].n37, apocalypse);
+		CalcNation (nations[0].n38, apocalypse);
+		CalcNation (nations[0].n39, apocalypse);
+		CalcNation (nations[0].n40, apocalypse);
+		CalcNation (nations[0].n41, apocalypse);
+		CalcNation (nations[0].n42, apocalypse);
+		CalcNation (nations[0].n43, apocalypse);
+		CalcNation (nations[0].n44, apocalypse);
+		CalcNation (nations[0].n45, apocalypse);
+		CalcNation (nations[0].n46, apocalypse);
+		CalcNation (nations[0].n47, apocalypse);
+		CalcNation (nations[0].n48, apocalypse);
+		CalcNation (nations[0].n49, apocalypse);
+		CalcNation (nations[0].n50, apocalypse);
+
+	}
+	public static void CalcThreat(Nation nation)
+	{
+		nation.threat = (((nation.science * nation.economy) * 1.1) +
+			((nation.military - nation.population) * 1.3) + 
+			((nation.truces - nation.takeovers) * 1.6));
+
+	}
+	}
+
+
---
+++ Apocalypse%2520Nations/Assets/nationData.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+//Author Tim Henders
+
+[System.Serializable]
+public class nationData {
+
+	public static nationData current;
+	public Nation n1;
+	public Nation n2;
+	public Nation n3;
+	public Nation n4;
+	public Nation n5;
+	public Nation n6;
+	public Nation n7;
+	public Nation n8;
+	public Nation n9;
+	public Nation n10;
+	public Nation n11;
+	public Nation n12;
+	public Nation n13;
+	public Nation n14;
+	public Nation n15;
+	public Nation n16;
+	public Nation n17;
+	public Nation n18;
+	public Nation n19;
+	public Nation n20;
+	public Nation n21;
+	public Nation n22;
+	public Nation n23;
+	public Nation n24;
+	public Nation n25;
+	public Nation n26;
+	public Nation n27;
+	public Nation n28;
+	public Nation n29;
+	public Nation n30;
+	public Nation n31;
+	public Nation n32;
+	public Nation n33;
+	public Nation n34;
+	public Nation n35;
+	public Nation n36;
+	public Nation n37;
+	public Nation n38;
+	public Nation n39;
+	public Nation n40;
+	public Nation n41;
+	public Nation n42;
+	public Nation n43;
+	public Nation n44;
+	public Nation n45;
+	public Nation n46;
+	public Nation n47;
+	public Nation n48;
+	public Nation n49;
+	public Nation n50;
+
+	public nationData(){
+		n1 = new Nation ();
+		n2 = new Nation ();
+		n3 = new Nation ();
+		n4 = new Nation ();
+		n5 = new Nation ();
+		n6 = new Nation ();
+		n7 = new Nation ();
+		n8 = new Nation ();
+		n9 = new Nation ();
+		n10 = new Nation ();
+		n11 = new Nation ();
+		n12 = new Nation ();
+		n13 = new Nation ();
+		n14 = new Nation ();
+		n15 = new Nation ();
+		n16 = new Nation ();
+		n17 = new Nation ();
+		n18 = new Nation ();
+		n19 = new Nation ();
+		n20 = new Nation ();
+		n21 = new Nation ();
+		n22 = new Nation ();
+		n23 = new Nation ();
+		n24 = new Nation ();
+		n25 = new Nation ();
+		n26 = new Nation ();
+		n27 = new Nation ();
+		n28 = new Nation ();
+		n29 = new Nation ();
+		n30 = new Nation ();
+		n31 = new Nation ();
+		n32 = new Nation ();
+		n33 = new Nation ();
+		n34 = new Nation ();
+		n35 = new Nation ();
+		n36 = new Nation ();
+		n37 = new Nation ();
+		n38 = new Nation ();
+		n39 = new Nation ();
+		n40 = new Nation ();
+		n41 = new Nation ();
+		n42 = new Nation ();
+		n43 = new Nation ();
+		n44 = new Nation ();
+		n45 = new Nation ();
+		n46 = new Nation ();
+		n47 = new Nation ();
+		n48 = new Nation ();
+		n49 = new Nation ();
+		n50 = new Nation ();
+	}
+
+
+	}
+
+

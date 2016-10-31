19b9b5e09b7309ebc0e9b54404100b6628a02183
Jsaller <JoshuaASaller@gmail.com>
2016-10-31 14:07:04 -0600

    no message

---
+++ Apocalypse Nations/Assets/Josh Map/Argentina.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Argentina : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseEnter()
+	{
+		Debug.Log("Main Argentina Map");
+	}
+
+	public void OnMouseDown()
+	{
+		Debug.Log ("Main Argentina Map Click");
+	}
+}

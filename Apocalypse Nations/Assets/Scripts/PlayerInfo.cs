c6065d2d91f3dda2eb3537e425a850c00db11d13
Oshea Brown <oshea.brown@gmail.com>
2016-12-11 19:08:38 -0700

    Changed player info panels to be displayed at all times on the left side of the screen. I also moved the player actions panel to be out of the way of any countries.

--- Apocalypse Nations/Assets/Scripts/PlayerInfo.cs
+++ Apocalypse Nations/Assets/Scripts/PlayerInfo.cs
@@ -4,29 +4,16 @@ using UnityEngine.UI;
 
 public class PlayerInfo : MonoBehaviour 
 {
-	//public Alliance alliance;
-	public Nation nation;
 	public Text playerInfoText;
-    public GameObject allianceObj;
-    Alliance alliance;
-    GameObject gameManager;
-	// Use this for initialization
-	void Start () 
-	{
-        
-        SetPanelInfo();	
-	}
-	
+	public Text title;
+    public Alliance alliance;
+
 	// Update is called once per frame
 	void Update () 
 	{
-        SetPanelInfo();
-        playerInfoText.text = "\n" + alliance.name + "\nPopulation: " + alliance.population + "\nMilitary: " + alliance.military +
-			"\nScience: " + alliance.science + "\nReligion: " + alliance.religion + "\nEconomy: " + alliance.economy;
+		playerInfoText.text = "Pop: " + alliance.population + "\t\tRel: " + alliance.religion + "\nSci: " + alliance.science +
+			"\t\tEcn: " + alliance.economy + "\nMil: " + alliance.military;
+
+		title.text = alliance.name + " Stats";
 	}
-
-    void SetPanelInfo() {
-        alliance = GameObject.FindObjectOfType<GameManager>().activeAlliance;
-
-    }
 }
